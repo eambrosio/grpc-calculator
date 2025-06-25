94e4cb02412448ab6bea05d35df6efac4181c502
Emilio Ambrosio <emilio.ambrosio@joyn.de>
2025-06-25 17:37:50 +0200

    add Admin service and state support

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+use std::sync;
+
 use proto::calculator_server::{Calculator, CalculatorServer};
+use tokio::sync::RwLock;
 use tonic::transport::Server;
+
+use crate::proto::admin_server::{Admin, AdminServer};
 
 mod proto {
     tonic::include_proto!("calculator");
@@ -8,9 +13,37 @@ mod proto {
         tonic::include_file_descriptor_set!("calculator_descriptor");
 }
 
+type State = sync::Arc<RwLock<u64>>;
 #[derive(Debug, Default)]
-struct CalculatorService {}
+struct CalculatorService {
+    state: State,
+}
 
+#[derive(Debug, Default)]
+struct AdminService {
+    state: State,
+}
+
+#[tonic::async_trait]
+impl Admin for AdminService {
+    async fn get_request_count(
+        &self,
+        _request: tonic::Request<proto::GetRequestCountRequest>,
+    ) -> Result<tonic::Response<proto::GetRequestCountResponse>, tonic::Status> {
+        let count = self.state.read().await;
+        let response = tonic::Response::new(proto::GetRequestCountResponse { count: *count });
+
+        Ok(response)
+    }
+}
+
+impl CalculatorService {
+    async fn increment_counter(&self) {
+        let mut count = self.state.write().await;
+        *count += 1;
+        println!("Request count: {}", *count);
+    }
+}
 #[tonic::async_trait]
 impl Calculator for CalculatorService {
     #[doc = " Adds two integers and returns the result."]
@@ -24,6 +57,7 @@ impl Calculator for CalculatorService {
             result: input.a + input.b,
         };
 
+        self.increment_counter().await;
         Ok(tonic::Response::new(response))
     }
 
@@ -41,6 +75,8 @@ impl Calculator for CalculatorService {
             result: input.a / input.b,
         };
 
+        self.increment_counter().await;
+
         Ok(tonic::Response::new(response))
     }
 }
@@ -49,7 +85,13 @@ impl Calculator for CalculatorService {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "127.0.0.1:50051".parse()?;
 
-    let calc = CalculatorService::default();
+    let state = State::default();
+    let calc = CalculatorService {
+        state: state.clone(),
+    };
+    let admin = AdminService {
+        state: state.clone(),
+    };
 
     let reflection_service = tonic_reflection::server::Builder::configure()
         .register_encoded_file_descriptor_set(proto::FILE_DESCRIPTOR_SET)
@@ -58,6 +100,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Server::builder()
         .add_service(reflection_service)
         .add_service(CalculatorServer::new(calc))
+        .add_service(AdminServer::new(admin))
         .serve(addr)
         .await?;
 
